06d3a9f830fecd2710c77788a6ff45fbc8d38358
francis-mbithi <frankmbithifm@gmail.com>
2019-12-17 15:47:35 +0300

    convert array to String

--- app/src/main/java/com/moringaschool/makeups/MainActivity.java
+++ app/src/main/java/com/moringaschool/makeups/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,11 +18,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
 
     @BindView(R.id.locationTextView) TextView mLocationTextView;
-
     @BindView(R.id.listView) ListView mListView;
 
-    private String[] makeup = new String[] {"Sweet Hereafter", "Cricket", "Hawthorne Fish House", "Viking Soul Food", "Red Square", "Horse Brass", "Dick's Kitchen", "Taco Bell", "Me Kha Noodle Bar", "La Bonita Taqueria", "Smokehouse Tavern", "Pembiche", "Kay's Bar", "Gnarly Grey", "Slappy Cakes", "Mi Mero Mole" };
-    private String[] cosmetics= new String[] {"Vegan Food", "Breakfast", "Fishs Dishs", "Scandinavian", "Coffee", "English Food", "Burgers", "Fast Food", "Noodle Soups", "Mexican", "BBQ", "Cuban", "Bar Food", "Sports Bar", "Breakfast", "Mexican" };
+    private String[] makeup = new String[] {"Face Primer", "BB Cream ", "Foundation", "Blush", "Glow Kit Highlighter", "Bronzer", "Setting Spray", "Setting Powder"};
+    private String[] cosmetics= new String[] {"Sephora", "Ulta", " Clinique", "Scandinavian", " Anastasia Beverly Hills", " Milani", "Amazon", " Lord & Taylor"  };
 
 
 
